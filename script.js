f246686684ad39239317e334dead7b573adcecbf
SickMyDucks <me@neilrichter.com>
2018-01-24 00:39:58 +0100

    Can now search with number

--- script.js
+++ script.js
@@ -1,29 +1,42 @@
 window.onload = function() {
     pkmnPicture = document.querySelector('.pokemon');
     pkmnName = document.querySelector('.name');
+    pkmnType = document.querySelector('.type');
     pkmnAttack = document.querySelector('.attack');
     pkmnDefense = document.querySelector('.defense');
     move0 = document.querySelector('#move0');
     move1 = document.querySelector('#move1');
     move2 = document.querySelector('#move2');
     move3 = document.querySelector('#move3');
+    document.querySelector('.buttons div:last-child').onclick = function() {
+        document.querySelector('.input').style.display = 'flex';
+    }
+    document.querySelector('.buttons div:first-child').onclick = function() {
+        document.querySelector('.input').style.display = 'none';
+    }
 
-    $.ajax({
-            method: 'get',
-            dataType: 'json',
-            url: './pokemon.json',
-            success: function(data) {
-                pokemon = data;
-                url = 'https://archives.bulbagarden.net/media/upload/thumb/e/ec/122Mr._Mime.png/120px-122Mr._Mime.png';
-                pkmnPicture.innerHTML = '<img src="'+url+'"/>';
-                pkmnName.innerHTML = pokemon[122].name;
-                pkmnAttack.innerHTML = 'Attack:' + pokemon[122].attack;
-                pkmnDefense.innerHTML = 'Defense:' + pokemon[122].defense;
-                for (var i in pokemon[122].moves) {
-                    console.log(i);
-                    document.querySelector('#move'+i).innerHTML = pokemon[122].moves[i];
-                }
-            }
-        });
-    
+    document.querySelector('form').onsubmit = function() {
+        console.log(this.querySelector('input').value);
+        chosenPokemon = this.querySelector('input').value;
+        url = 'http://www.pokestadium.com/assets/img/sprites/official-art/large/'+pokemon[chosenPokemon].name.toLowerCase()+'.png';
+        pkmnPicture.innerHTML = '<img src="'+url+'"/>';
+        pkmnName.innerHTML = pokemon[chosenPokemon].name;
+        pkmnType.innerHTML = '<strong>Type: </strong>'+pokemon[chosenPokemon].type;
+        pkmnAttack.innerHTML = 'Attack:' + pokemon[chosenPokemon].attack;
+        pkmnDefense.innerHTML = 'Defense:' + pokemon[chosenPokemon].defense;
+        for (var i in pokemon[chosenPokemon].moves) {
+            document.querySelector('#move'+i).innerHTML = pokemon[chosenPokemon].moves[i];
+        }
+        document.querySelector('.input').style.display = 'none';
+        return false;
+    }
 };
+pokemon = $.ajax({
+    method: 'get',
+    dataType: 'json',
+    url: './pokemon.json',
+    success: function(data) {
+        pokemon = data;
+        return pokemon;
+    }
+});
